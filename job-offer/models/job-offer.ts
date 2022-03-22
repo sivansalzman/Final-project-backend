8016de0cbb28094839099204456080c69f317d33
SivanSalzmann <sivansalzmann10@gmail.com>
2022-03-22 03:55:27 +0200

    Update job-offer.ts

--- job-offer/models/job-offer.ts
+++ job-offer/models/job-offer.ts
@@ -1,17 +1,54 @@
 import { Schema, model } from "mongoose";
 
+const experienceSchema = new Schema({
+  company_id: { type: String },
+  company_name: { type: String },
+  company_founded: { type: String },
+  company_industry: { type: String },
+  company_size: { type: String },
+  current_job: { type: Boolean },
+  company_location_name: { type: String },
+  company_location_country: { type: String },
+  company_location_continent: { type: String },
+  end_date: { type: String },
+  start_date: { type: String },
+  title_name: { type: String },
+  title_role: { type: String },
+  title_levels: { type: [String] },
+});
+
+const educationSchema = new Schema({
+  school_name: { type: String },
+  school_type: { type: String },
+  degrees: { type: [String] },
+  start_date: { type: String },
+  end_date: { type: String },
+  majors: { type: [String] },
+  minors: { type: [String] },
+  gpa: { type: Number },
+});
+
 const jobOfferSchema = new Schema(
   {
     job_offer_ID: { type: String },
-    job_company_name: { type: String },
+    full_name: { type: String },
+    first_name: { type: String },
+    last_name: { type: String },
+    gender: { type: String },
+    birth_year: { type: String },
+    birth_date: { type: String },
     industry: { type: String },
     job_title: { type: String },
     job_title_role: { type: String },
     job_title_sub_role: { type: String },
-    role_description: { type: String },
+    job_title_levels: { type: [String] },
+    job_company_id: { type: String },
+    job_company_name: { type: String },
     job_start_date: { type: String },
-    years_experience: { type: String },
+    interests: { type: [String] },
     skills: { type: [String] },
+    experience: { type: [experienceSchema] },
+    education: { type: [educationSchema] },
     status: { type: String },
     candidates_id: { type: [String] },
   },
